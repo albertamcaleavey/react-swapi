cf659315f5c50ec5e4e04c87873688a254867c3e
Alberta McAleavey <a.mcaleavey24@gmail.com>
2022-03-17 17:48:24 -0400

    complete display pilot list functionality

--- src/components/PilotList/PilotList.jsx
+++ src/components/PilotList/PilotList.jsx
@@ -1,38 +1,36 @@
 import React, { useState, useEffect } from "react";
+import StarshipDetails from "../../pages/StarshipDetails/StarshipDetails";
 import { getPilots } from "../../services/sw-api";
+import { useLocation } from "react-router-dom";
 
 const PilotList = (props) => {
-  const [pilotList, setPilotList] = useState([
-    "https://swapi.dev/api/people/13/", 
-    "https://swapi.dev/api/people/14/", 
-    "https://swapi.dev/api/people/25/", 
-    "https://swapi.dev/api/people/31/",
-  ])
+  const [pilotList, setPilotList] = useState([])
+
+  let location = useLocation()
   
   useEffect(()=> {
-    getPilots(pilotList).then(pilots => setPilotList(pilots))
-  }, [pilotList])
+    getPilots(location.state.starship.pilots).then(pilots => setPilotList(pilots))
+  }, [])
   
+  console.log(location)
 
   return (  
     <>
-    {pilotList.name ?
+    {pilotList.length ?
     <>
-    
-    </>
-    
-    
-    
-    
-    }
     <p>PILOTS: </p>
     <ul>
       {pilotList.map((pilot, index)=> (
-        <li key={pilot.index}>{pilot.name}</li>
+        <li key={pilot.name}>{pilot.name}</li>
       ))}
     </ul>
     </>
+    :
+    <>
+    <p>No pilots</p>
+    </>
+    }
+    </>
   );
 }
- 
 export default PilotList;
